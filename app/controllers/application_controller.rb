748b5dc7a10554e62dbbdda4361826073221edb6
AfiqIzzan <afiqizzan16@gmail.com>
2023-12-28 10:20:27 +0800

    validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
+	before_action :authenticate_user!, except: [:new, :create]
 
 	def application
 		@id = Feed.first.id
 		puts "id#{@id}"
+	end
+
+	private 
+
+	def authenticate_user!
+		if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path, notice: 'Please sign in.'
+    end
 	end
 
 
